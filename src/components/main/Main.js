fd2f0138359bbf65d418a3645cf7202043b6f230
Justin <justinaiello@gmail.com>
2019-10-30 19:23:56 -0400

    Created switch btton files **note taking dinner break

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -10,13 +10,56 @@ function Main() {
       <Header />
       <Breadcrumbs />
       <StyledSection>
-        <Paper />
-        <Paper />
-        <Paper />
-        <Paper />
+        {data.map(item => (
+          <Paper
+            heading={item.heading}
+            subTop={item.subTop}
+            contentTop={item.contentTop}
+            subBottom={item.subBottom}
+            contentBottom={item.contentBottom}
+            hr={item.hr}
+            key={item.heading}
+            toolTipTop={item.toolTipTop}
+            toolTipBottom={item.toolTipBottom}
+          />
+        ))}
       </StyledSection>
     </StyledMain>
   );
 }
 
 export default Main;
+
+const data = [
+  {
+    heading: 'Account Settings',
+    subTop: 'Default Currency',
+    contentTop:
+      'This is the default currency that all of your valuations will be translated to.',
+    subBottom: 'Accounting Methodology',
+    contentBottom:
+      'This is the methodology in which your tax and accounting documentation will be prepared.',
+    hr: true,
+    toolTipBottom: true,
+  },
+  {
+    heading: 'Notification Settings',
+    subTop: 'SMS Notifications',
+    contentTop: 'We will push all notifications to your mobile devices.',
+    subBottom: 'Email Notifications',
+    contentBottom: 'Your email and phone number will not be searchable.',
+    hr: true,
+  },
+  {
+    heading: 'Privacy Settings',
+    subTop: 'Make Profile Private',
+    contentTop:
+      'Your profile with your wallet info will not be public or searchable.',
+    subBottom: 'Search Privacy',
+    contentBottom:
+      'This is the methodology in which your tax and accounting documentation will be prepared.',
+    hr: true,
+    toolTipTop: true,
+    toolTipBottom: true,
+  },
+];
--- src/components/paper/Paper.js
+++ src/components/paper/Paper.js
@@ -1,24 +1,31 @@
 import React from 'react';
 import { StyledPaper, StyledSection } from './PaperStyles';
+import { TiInfoLarge } from 'react-icons/ti';
 
 function Paper(props) {
   return (
     <StyledPaper>
-      <h3 className="paper-heading">Placeholder</h3>
+      <h3 className="paper-heading">{props.heading}</h3>
       <StyledSection>
         <div className="top">
-          <h4 className="sub-heading">Placeholder</h4>
-          <div className="button-container">Placeholder</div>
+          <h4 className="sub-heading">
+            {props.subTop}{' '}
+            {props.toolTipTop && <TiInfoLarge className="tooltip" />}
+          </h4>
+          <div className="button-container">Button</div>
         </div>
         <p className="info-block">{props.contentTop}</p>
       </StyledSection>
-      <hr className="divider" />
+      {props.hr && <hr className="divider" />}
       <StyledSection>
         <div className="top">
-          <h4 className="sub-heading">Placeholder</h4>
-          <div className="button-container">Placeholder</div>
+          <h4 className="sub-heading">
+            {props.subBottom}{' '}
+            {props.toolTipBottom && <TiInfoLarge className="tooltip" />}
+          </h4>
+          <div className="button-container">button</div>
         </div>
-        <p className="info-block">Placeholder</p>
+        <p className="info-block">{props.contentBottom}</p>
       </StyledSection>
     </StyledPaper>
   );
